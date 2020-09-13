f6d66b19d159ef287a4cf7c6bceef54e8f19756e
Nuelthesupreme <nuelthesupreme@gmail.com>
2020-09-13 11:25:18 +0100

    created the router and updated the config server and package.json

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,9 @@
-const PORT = process.env.PORT || 8000;
+export const PORT = process.env.PORT || 8000;
 
-const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout-db';
+export const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout-db';
 
-const MONGOOSE_OPTIONS = {
+export const MONGOOSE_OPTIONS = {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true,
-};
-
-export default {
-  PORT,
-  DB_URI,
-  MONGOOSE_OPTIONS,
 };
