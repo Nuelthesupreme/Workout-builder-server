8e7d5ea13f9670d9823f7acd5e6ae3c182d866c9
Nuelthesupreme <nuelthesupreme@gmail.com>
2020-09-23 12:15:26 +0100

    added a save route and a get all workout route

--- src/routes/api-routes.js
+++ src/routes/api-routes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { get } from 'mongoose';
 import db from '../models';
 
 const router = express.Router();
@@ -118,6 +119,27 @@ const deleteWorkout = async (req, res) => {
   }
 };
 
+const saveWorkout = async (req, res) => {
+  try {
+    const { id: workoutId } = req.params;
+    const { id: userId } = req.user;
+    const data = await db.workout.create({ _id: workoutId, userId });
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+const getAllWorkouts = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const data = await db.workout.find({ userId: id });
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 const getExercisesForMuscleGroup = async (req, res) => {
   try {
     const { id: muscleGroupId } = req.params;
@@ -145,7 +167,8 @@ router.post('/workouts', createWorkout);
 router.get('/workouts/:id', getWorkout);
 router.put('/workouts/:id', updateWorkout);
 router.delete('/workouts/:id', deleteWorkout);
-
+router.post("/saveWorkout", saveWorkout);
+router.get("/myWorkout", getAllWorkouts);
 router.get('/muscleGroups', getMuscleGroups);
 router.get('/muscleGroups/:id/exercises', getExercisesForMuscleGroup);
 
