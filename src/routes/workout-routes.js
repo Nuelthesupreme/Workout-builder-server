d6eec1c222b3b9b1224cf810e07ec1021d195703
kepop1 <kepop123@gmail.com>
2020-09-25 19:10:17 +0100

    Adjusted the workout routes and added functionality for update

--- src/routes/workout-routes.js
+++ src/routes/workout-routes.js
@@ -5,12 +5,18 @@ const router = express.Router();
 
 const getUserWorkout = async (req, res) => {
   try {
-    const { user: { id } } = req;
-    console.log(req.user);
-    const allWorkouts = await db.workout.find({ userId: id }).sort({ createdAt: -1 });
-    res.json({ allWorkouts });
+    const { id } = req.user;
+
+    const allWorkouts = await db.Workout.find({ userId: id })
+      .populate([{ path: 'exercises.exercise' }, { path: 'muscleGroup' }])
+      .sort({
+        createdAt: -1,
+      });
+
+    res.status(200).json({ success: true, allWorkouts });
   } catch (error) {
     res.status(500).json({
+      success: false,
       error: error.message,
     });
   }
@@ -19,43 +25,71 @@ const getUserWorkout = async (req, res) => {
 const getUserWorkOutById = async (req, res) => {
   try {
     const { id } = req.params;
-    const workout = await db.workout.findOne({ _id: id });
-    res.status(201).json({
+
+    const workout = await db.Workout.findOne({ _id: id }).populate([
+      { path: 'exercises.exercise' },
+      { path: 'muscleGroup' },
+    ]);
+
+    res.status(200).json({
       success: true,
       workout,
     });
   } catch (error) {
     res.status(500).json({
+      success: false,
       error: error.message,
     });
+  }
+};
+
+const createUserWorkOut = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const workout = req.body;
+
+    const newWorkout = await db.Workout.create({ ...workout, userId: id });
+
+    res.status(201).json({ success: true, workout: newWorkout });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
   }
 };
 
 const deleteUserWorkOut = async (req, res) => {
   try {
     const { id } = req.params;
-    await db.workout.findByIdAndDelete(id);
-    res.status(201).json({
+
+    await db.Workout.findByIdAndDelete(id);
+
+    res.status(200).json({
       success: true,
     });
   } catch (error) {
     res.status(500).json({
+      success: false,
       error: error.message,
     });
   }
 };
-const createUserWorkOut = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  const { id } = req.user;
-  const { workout } = req.body;
-  res.send({ id, workout });
-};
-const updateUserWorkOut = async () => { };
 
-router.get('/userworkouts', getUserWorkout);
-router.get('/userworkouts/:id', getUserWorkOutById);
-router.post('/userworkouts', deleteUserWorkOut);
-router.put('/userworkouts', createUserWorkOut);
-router.delete('/userworkouts/:id', updateUserWorkOut);
+const updateUserWorkOut = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { body } = req;
+
+    const data = await db.Workout.findByIdAndUpdate(id, body, { new: true });
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+router.get('/workouts', getUserWorkout);
+router.get('/workouts/:id', getUserWorkOutById);
+router.post('/workouts', createUserWorkOut);
+router.put('/workouts/:id', updateUserWorkOut);
+router.delete('/workouts/:id', deleteUserWorkOut);
 
 export default router;
